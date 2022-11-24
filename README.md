ccbc802649c918019b0387b4ab9d6c57aa945f19
James <james.jcarline@gmail.com>
2022-11-24 22:43:49 +0000

    Implement #map methods and do loops to create method

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # caesar_cipher
+
+This project will reinforce my Ruby array methods knowlege. I will make use of the #map
+method among others.
--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,8 +1,18 @@
 require 'pry-byebug'
 
 def caesar_cipher(message, shiftValue)
-    string_array = message.chars.map(&:ord)
-    binding.pry
+    string_array = message.chars.map {|c| c.ord}
+    shifted_array = string_array.map {|c| c + shiftValue}
+    corrected_array = shifted_array.map do |item|
+        if item > 122
+            item - 26
+        else
+            item
+        end
+    end
+    result_array = corrected_array.map {|c| c.chr}.join
+    puts result_array
+    return result_array
 end
 
-caesar_cipher("test this", 6)
+caesar_cipher("Hello", 6)
